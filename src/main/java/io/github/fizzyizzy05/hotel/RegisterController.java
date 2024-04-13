4dff3b4c077907f0450f73d3044f3954a38d655f
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-13 18:34:07 +0100

    Add RegisterController

---
+++ src/main/java/io/github/fizzyizzy05/hotel/RegisterController.java
@@ -0,0 +1,12 @@
+package io.github.fizzyizzy05.hotel;
+
+import javafx.fxml.FXML;
+import java.io.IOException;
+
+public class RegisterController {
+
+    @FXML
+    private void register() throws IOException {
+        System.out.println("Registering...");
+    }
+}
