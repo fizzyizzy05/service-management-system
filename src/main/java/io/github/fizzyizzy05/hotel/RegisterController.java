a2b4a8dbdc31e6a6f1af15127b7199efa1845975
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-13 22:06:04 +0100

    Add function to Register button

--- src/main/java/io/github/fizzyizzy05/hotel/RegisterController.java
+++ src/main/java/io/github/fizzyizzy05/hotel/RegisterController.java
@@ -2,16 +2,18 @@ package io.github.fizzyizzy05.hotel;
 
 import javafx.fxml.FXML;
 import java.io.IOException;
+import javafx.scene.control.TextField;
 
 public class RegisterController {
 
-    @FXML
-    private void register() throws IOException {
-        System.out.println("Registering");
+    @FXML private TextField firstNameIn;
+
+    @FXML private void register() throws IOException {
+        System.out.println("abc");
+        System.out.println(firstNameIn.getText());
     }
 
-    @FXML
-    private void goLogin() throws IOException{
+    @FXML private void goLogin() throws IOException{
         App.setRoot("login");
     }
 }
