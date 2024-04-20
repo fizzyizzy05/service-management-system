727934a6c3e04033e013e4502a5f85e88fb0d0e7
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-20 21:09:11 +0100

    Add JDBC to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.github.fizzyizzy05.hotel {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens io.github.fizzyizzy05.hotel to javafx.fxml;
     exports io.github.fizzyizzy05.hotel;
