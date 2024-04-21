197cf1a400800da0fd6eba24d1897eeefc988b31
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-21 22:04:26 +0100

    Update comments

--- src/main/java/io/github/fizzyizzy05/hotel/LoginController.java
+++ src/main/java/io/github/fizzyizzy05/hotel/LoginController.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 
 public class LoginController {
 
+    // Validate the user
     @FXML
     private void login() throws IOException {
         System.out.println("Logging in!");
     }
 
+    // Change the screen to login
     @FXML
     private void goRegister() throws IOException {
         App.setRoot("register");
