5af9e5e7bf7ff3f4f888b19163f6c9cdbef25c10
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-22 12:59:28 +0100

    Update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.github.fizzyizzy05.hotel {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
     opens io.github.fizzyizzy05.hotel to javafx.fxml;
     exports io.github.fizzyizzy05.hotel;
