dc29caf2c3abb8f205390d8d7e48a76c2269e939
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-22 01:06:19 +0100

    Add code to check password

--- src/main/java/io/github/fizzyizzy05/hotel/LoginController.java
+++ src/main/java/io/github/fizzyizzy05/hotel/LoginController.java
@@ -1,17 +1,27 @@
 package io.github.fizzyizzy05.hotel;
 
+import java.sql.*;
 import javafx.fxml.FXML;
 import java.io.IOException;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 
 public class LoginController {
+    // Import the widgets used in the FXML file
+    @FXML private TextField emailIn;
+    @FXML private PasswordField passIn;
 
     // Validate the user
     @FXML
-    private void login() throws IOException {
-        System.out.println("Logging in!");
+    private void login() throws IOException, SQLException {
+        Connection dbConnection = App.getConnection();
+        ResultSet accDetails = dbConnection.createStatement().executeQuery("SELECT password FROM Users WHERE email = '" + emailIn.getText() + "';");
+        if (passIn.getText().equals(accDetails.getString("password"))) {
+            System.out.println("Logging in");
+        }
     }
 
-    // Change the screen to login
+    // Change the screen to register
     @FXML
     private void goRegister() throws IOException {
         App.setRoot("register");
