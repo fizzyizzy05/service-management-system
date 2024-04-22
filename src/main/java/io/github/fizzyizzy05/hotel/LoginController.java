852ad7622560ec524f43413e3a48b9cca2103df0
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-22 19:27:07 +0100

    Properly close the connection to the database

--- src/main/java/io/github/fizzyizzy05/hotel/LoginController.java
+++ src/main/java/io/github/fizzyizzy05/hotel/LoginController.java
@@ -18,6 +18,7 @@ public class LoginController {
         ResultSet accDetails = dbConnection.createStatement().executeQuery("SELECT password, firstName, lastName, ID FROM Users WHERE email = '" + emailIn.getText() + "';");
         if (passIn.getText().equals(accDetails.getString("password"))) {
             App.getAccountManager().login(emailIn.getText(), accDetails.getString("firstName"), accDetails.getString("lastName"), accDetails.getInt("ID"), accDetails.getString("password"));
+            dbConnection.close();
             App.setRoot("admin");
         } else if (accDetails.getString("password") == null) {
             Alert alert = new Alert(AlertType.ERROR);
