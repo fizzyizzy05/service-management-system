b82cbc694c1a37b12e0cc569e1800de99cda282c
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-24 17:45:58 +0100

    service: initial commit

---
+++ src/main/java/io/github/fizzyizzy05/hotel/Service.java
@@ -0,0 +1,13 @@
+package io.github.fizzyizzy05.hotel;
+
+public class Service {
+    protected int price;
+    protected String name;
+
+    public Service(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    
+}
