92aa4dade1562cd511cc53bcd087f135f0718456
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-25 05:36:57 +0100

    AdminController: remove unused imports

--- src/main/java/io/github/fizzyizzy05/hotel/AdminController.java
+++ src/main/java/io/github/fizzyizzy05/hotel/AdminController.java
@@ -9,8 +9,6 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.*;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.TitledPane;
