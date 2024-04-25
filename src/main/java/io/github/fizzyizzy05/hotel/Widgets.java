bfebdfa6486b64b166a273679240a46216de6fc8
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-25 03:52:58 +0100

    Widgets: add function to format hourly rate

--- src/main/java/io/github/fizzyizzy05/hotel/Widgets.java
+++ src/main/java/io/github/fizzyizzy05/hotel/Widgets.java
@@ -10,12 +10,25 @@ public class Widgets {
         HBox widget = new HBox();
         Label idLbl = new Label(Integer.toString(id));
         Label nameLbl = new Label(name);
-        Label rateLbl = new Label(Integer.toString(hourlyRate));
+        Label rateLbl = new Label(formatPrice(hourlyRate));
         // Styling
         widget.setPadding(new Insets(5, 10, 5, 10));
         nameLbl.setPadding(new Insets(0, 5, 0, 5));
         widget.getChildren().addAll(idLbl, nameLbl, rateLbl);
         // Return this custom widget
         return widget;
+    }
+
+    public static String formatPrice(int rate) {
+        String outputRate;
+        Double doubleRate = Double.valueOf(rate) / 100;
+        if (rate >= 100) {
+            outputRate = "£" + String.format("%.2f", doubleRate) + " per hour";
+        } else if (rate < 100) {
+            outputRate = String.format("%.2f", doubleRate) + "p per hour";
+        } else {
+            outputRate = "Free";
+        }
+        return outputRate;
     }
 }
