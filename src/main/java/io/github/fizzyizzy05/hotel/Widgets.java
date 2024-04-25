71cfbe04e9e17637a69bb8b0923e4eba04e16c3c
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-25 03:36:33 +0100

    Widgets: serviceWidget styling

--- src/main/java/io/github/fizzyizzy05/hotel/Widgets.java
+++ src/main/java/io/github/fizzyizzy05/hotel/Widgets.java
@@ -1,5 +1,6 @@
 package io.github.fizzyizzy05.hotel;
 
+import javafx.geometry.Insets;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Label;
 
@@ -9,6 +10,8 @@ public class Widgets {
         Label idLbl = new Label(Integer.toString(id));
         Label nameLbl = new Label(name);
         Label rateLbl = new Label(Integer.toString(hourlyRate));
+        widget.setPadding(new Insets(5, 10, 5, 10));
+        nameLbl.setPadding(new Insets(0, 5, 0, 5));
         widget.getChildren().addAll(idLbl, nameLbl, rateLbl);
         return widget;
     }
