15b487f5e7acddc7a1678bcc3fa2b177dfc61348
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-25 10:24:17 +0100

    NewAppointment: make fields mandatory

--- src/main/java/io/github/fizzyizzy05/hotel/NewAppointmentController.java
+++ src/main/java/io/github/fizzyizzy05/hotel/NewAppointmentController.java
@@ -4,10 +4,12 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Alert.AlertType;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -37,25 +39,32 @@ public class NewAppointmentController {
     }
 
     @FXML public void makeAppointment() throws IOException, SQLException {
-        String selected = (String) serviceBox.getSelectionModel().getSelectedItem();
-        int serviceID = Integer.parseInt(selected.substring(0, selected.indexOf(":")));
+        if (titleIn.getText().equals("") || dateIn.getValue().equals(null) || hourIn.getText().equals("") || minIn.getText().equals("")) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("Please fill in the form properly");
+            alert.showAndWait();
+        } else {
+            String selected = (String) serviceBox.getSelectionModel().getSelectedItem();
+            int serviceID = Integer.parseInt(selected.substring(0, selected.indexOf(":")));
 
-        Connection dbConnection = App.getConnection();
-        Statement stmt = dbConnection.createStatement();
-        ResultSet appointments = stmt.executeQuery("SELECT * FROM Appointments;");
-        while (appointments.next()) {
-            maxID = appointments.getInt("ID") + 1;
+            Connection dbConnection = App.getConnection();
+            Statement stmt = dbConnection.createStatement();
+            ResultSet appointments = stmt.executeQuery("SELECT * FROM Appointments;");
+            while (appointments.next()) {
+                maxID = appointments.getInt("ID") + 1;
+            }
+            stmt.executeUpdate("INSERT INTO Appointments" + 
+                                "(ID, title, desc, service, time, customer) VALUES "
+                                + "('" + maxID + "', '"
+                                + titleIn.getText() + "', '"
+                                + descIn.getText() + "', '"
+                                + serviceID + "', '"
+                                + dateIn.getValue() + " " + hourIn.getText() + ":" + minIn.getText() + "', '"
+                                + App.getAccountManager().getID() + "');"
+            );
+            stmt.close();
+            dbConnection.close();
         }
-        stmt.executeUpdate("INSERT INTO Appointments" + 
-                            "(ID, title, desc, service, time, customer) VALUES "
-                            + "('" + maxID + "', '"
-                            + titleIn.getText() + "', '"
-                            + descIn.getText() + "', '"
-                            + serviceID + "', '"
-                            + dateIn.getValue() + " " + hourIn.getText() + ":" + minIn.getText() + "', '"
-                            + App.getAccountManager().getID() + "');"
-        );
-        stmt.close();
-        dbConnection.close();
     }
 }
