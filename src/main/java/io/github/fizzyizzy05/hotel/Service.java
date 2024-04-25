047ff59dd416abc49aa9b98a4aba89a384515683
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-25 02:38:23 +0100

    Service: add id

--- src/main/java/io/github/fizzyizzy05/hotel/Service.java
+++ src/main/java/io/github/fizzyizzy05/hotel/Service.java
@@ -2,9 +2,10 @@ package io.github.fizzyizzy05.hotel;
 
 public class Service {
     protected int price;
+    protected int id;
     protected String name;
 
-    public Service(String name, int price) {
+    public Service(int id, String name, int price) {
         this.name = name;
         this.price = price;
     }
