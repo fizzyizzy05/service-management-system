9dbd195e5601bc6d9e814daa78e12912b95e8f69
Isabelle Jackson <izzyjackson05@outlook.com>
2024-04-25 03:38:11 +0100

    Widgets: add comments

--- src/main/java/io/github/fizzyizzy05/hotel/Widgets.java
+++ src/main/java/io/github/fizzyizzy05/hotel/Widgets.java
@@ -6,13 +6,16 @@ import javafx.scene.control.Label;
 
 public class Widgets {
     public static HBox serviceWidget(int id, String name, int hourlyRate) {
+        // Content
         HBox widget = new HBox();
         Label idLbl = new Label(Integer.toString(id));
         Label nameLbl = new Label(name);
         Label rateLbl = new Label(Integer.toString(hourlyRate));
+        // Styling
         widget.setPadding(new Insets(5, 10, 5, 10));
         nameLbl.setPadding(new Insets(0, 5, 0, 5));
         widget.getChildren().addAll(idLbl, nameLbl, rateLbl);
+        // Return this custom widget
         return widget;
     }
 }
